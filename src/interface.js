2a56a684028bd0c1d444b027c7a4103c9ca738f6
sa <psa5013@hanmail.com>
2021-05-03 17:45:19 +0900

    interface 사용 기본

---
+++ src/interface.js
@@ -0,0 +1,24 @@
+var Circle = /** @class */ (function () {
+    function Circle(radius) {
+        this.radius = radius;
+    }
+    // 너비를 가져오는 함수를 구현합니다.
+    Circle.prototype.getArea = function () {
+        return this.radius * this.radius * Math.PI;
+    };
+    return Circle;
+}());
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}());
+var shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach(function (shape) {
+    console.log(shape.getArea());
+});
