7559d557044f87b11da91f06a4f3f18fb85ee233
sa <psa5013@hanmail.com>
2021-05-03 17:47:51 +0900

    일반 객체를(class아닌) interface로 타입 설정

---
+++ src/interface2.js
@@ -0,0 +1,10 @@
+var person = {
+    name: '김사람'
+};
+var expert = {
+    name: '김개발',
+    skills: ['javascript', 'react']
+};
+var people = [person, expert];
+var color = 'red';
+var colors = ['red', 'orange'];
