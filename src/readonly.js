985dc270d79746e2eca36f2f1e5d816c8ccca746
sanga99 <65518048+sanga99@users.noreply.github.com>
2021-05-12 16:21:53 +0900

    Create readonly.js

---
+++ src/readonly.js
@@ -0,0 +1,29 @@
+
+// github에서 따로 추가된 파일입니다. 
+
+/*
+  [typescipt - 접근제한자 중 readonly]
+  readonly 키워드는 읽기 전용 속성이다. 
+  이는 변수를 설정할 때와 생성자(constructor)에서만 할 수 있다.
+*/
+
+
+class Face {
+  
+  readonly vertex: number = 3;
+  readonly edge: number;
+
+  constructor(edge: number){
+    this.edge = edge;
+  }
+  
+  public eddEdge() {
+    this.edge++;        // TS2540: Cannot assign to 'edge' because it is a read-only
+  } 
+}
+
+
+const face = new Face(5);
+console.log(face.edge)    // 5
+console.log(face.vertex)  // 3
+
